8f26a83bf3c40d5d981f523839de92971c94ec14
DLeaguer <immostlikely@hotmail.com>
2018-06-22 13:08:25 -1000

    completed up to 4. count thumbs up

--- js/app.js
+++ js/app.js
@@ -1,12 +1,44 @@
 // 1. Add an event to the 'See Details' content that will create an alert box that says 'Not Available in Hawaii.' after clicking on See Details.
-
+details.addEventListener('click', seeDetails);
+function seeDetails() {
+    alert('Not Available in Hawaii.');
+}
 
 //2. Add an event to the div element with the id of 'name1' that will show/hide the description ('descrip1') after hovering over Air Jordan II.
+var getName1 = document.getElementById('name1');
+getName1.addEventListener('mouseover', showName1Desc);
 
+function showName1Desc(){
+    var getDescrip1 = document.getElementById('descrip1');
+    if(getDescrip1.style.display === 'block'){
+        getDescrip1.style.display = 'none';
+    }
+    else {
+        getDescrip1.style.display = 'block';
+    }
+}
 
 //3. Add an event to the div element with the id of 'name2' that will show/hide the following description after clicking on the element.
 //'The instantly recognizable Jumpman silhouette made its debut with the Air Jordan 3 during Michael Jordan's 1987-88 NBA season.'
+// var getName2 = document.getElementById('name2');
+var descrip2 = document.createElement('div');
+descrip2.id = 'descrip2';
+descrip2.innerHTML = 'The instantly recognizable Jumpman silhouette made its debut with the Air Jordan 3 during Michael Jordan\'s 1987-88 NBA season.';
+descrip2.style.display = 'none';
+name2.appendChild(descrip2);
+name2.addEventListener('click', showDescript2);
 
+function showDescript2() {
+    var getDescrip2 = document.getElementById('descrip2');
+    if(getDescrip2.style.display === 'block'){
+        console.log('none');
+        getDescrip2.style.display = 'none';
+    }
+    else {
+        console.log('block');
+        getDescrip2.style.display = 'block';
+    }
+}
 
 //You'll need to:
 // create a div element with an id of 'descrip2'
@@ -14,7 +46,20 @@
 
 
 //4. Add events to the all the thumbs up icon that will add a count for each time the icon is clicked on for any shoe.
+for(var i = 0; i < social.length; i++) {
+    var getThumbs = document.getElementsByClassName('far fa-thumbs-up')[i];
+    var thumbCount = document.createElement('div');
+    thumbCount.className = 'thumbCount';
+    thumbCount.innerHTML = '0';
+    getThumbs.appendChild(thumbCount);
+    
+    getThumbs.addEventListener('click', addThumbsUp);
+}
 
+function addThumbsUp() {
+    var countThumbs = this.querySelectorAll('.thumbCount')[0];
+    countThumbs.innerHTML++;
+}
 
 //5. Add an event to the plus icon that will increment the price of the Air Jordan V shoe each time the icon is clicked on.
 
